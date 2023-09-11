016880eb65ec0c07359c843b488c1cefca6e1b71
mad-dogy <120950389+mad-dogy@users.noreply.github.com>
2023-09-11 13:40:54 +0300

    start create layout of profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -2,6 +2,12 @@ import styles from './ProfilePage.module.scss';
 
 export const ProfilePage = (): JSX.Element => (
   <div className={styles.profile}>
-    Profile
+    <div className={styles.profile__inner}>
+      <h4>Personal account</h4>
+      <div className={styles['profile__info-block']}>
+        <h6>Personal info</h6>
+        <div className={styles['personal-info']} />
+      </div>
+    </div>
   </div>
 );
