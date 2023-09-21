56075bc1c40c819e05fa92790ab6d7c61f1c9110
Lola <120950389+mad-dogy@users.noreply.github.com>
2023-09-21 15:31:31 +0300

    highlight edit control panel in a separate component and delete unused component

---
+++ src/components/EditControlPanel/EditControlPanel.tsx
@@ -0,0 +1,35 @@
+import EditIcon from '@mui/icons-material/Edit';
+import { Button } from '../UI/Button/Button';
+import styles from './EditControlPanes.module.scss';
+
+export const EditControlPanel = (props: any) => {
+  const {
+    isEdit, onEditBtnClick, onCancelBtnClick, onSaveBtnClick,
+  } = props;
+
+  return (
+    <div>
+      {isEdit
+        ? (
+          <div className={styles['confirm-edit-btns']}>
+            <Button
+              className="button_small"
+              variant="outlined"
+              onClick={onSaveBtnClick}
+            >
+              Save
+            </Button>
+            <Button
+              className="button_small"
+              variant="outlined"
+              color="warning"
+              onClick={onCancelBtnClick}
+            >
+              Cancel
+            </Button>
+          </div>
+        )
+        : <EditIcon fontSize="medium" className={styles.edit_btn} onClick={onEditBtnClick} />}
+    </div>
+  );
+};
