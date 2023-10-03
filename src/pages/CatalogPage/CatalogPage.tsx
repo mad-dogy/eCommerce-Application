92a37878a9b82f327ee2d4f20549a2456dd244ae
mad-dogy <120950389+mad-dogy@users.noreply.github.com>
2023-10-03 21:29:19 +0300

    create smth

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,8 +1,10 @@
 import { FilterPanel } from '../../components/FilterPanel/FilterPanel';
+import { ProductsContainer } from '../../components/ProductsContainer/ProductsContainer';
 import styles from './CatalogPage.module.scss';
 
 export const CatalogPage = () => (
   <div className={styles.catalog}>
     <FilterPanel />
+    <ProductsContainer products="" />
   </div>
 );
